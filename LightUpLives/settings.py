d8336819236cdb97417be4192ac6742c8b188fc0
istabrak001 <istabrakmohamed3@gmail.com>
2025-03-29 11:10:15 +0200

    from user to users

--- LightUpLives/settings.py
+++ LightUpLives/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user.apps.UserConfig',
+    'users.apps.UsersConfig',
     'rest_framework',
 ]
 
--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.core.validators import RegexValidator
 
 # Create your models here.
 
-class User(models.Model):
+class Users(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=254, unique=True)
