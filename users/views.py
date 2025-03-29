08b64450638149f693ba94b5f36f516954697d5e
istabrak001 <istabrakmohamed3@gmail.com>
2025-03-29 17:06:02 +0200

    initial user templates (base, home, login, register)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import path
+from django.views.generic import View
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+def login(request):
+    return render(request, 'users/login.html')
+
+def register(request):
+    return render(request, 'users/register.html')
+
