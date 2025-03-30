5edf52a749c699f3484fe9fe50a78255a836f4e4
istabrak001 <istabrakmohamed3@gmail.com>
2025-03-30 10:13:15 +0200

    Add user model validations, update sign up template, and modify views

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator 
+import re
+
 
 
 
@@ -11,36 +13,40 @@ class Users(models.Model):
             regex=r'^[a-zA-Z]+$',
             message='First name must contain only letters'
         )
-    ])
+    ],db_column="first_name"
+    )
 
     last_name = models.CharField(max_length=30, validators=[
         RegexValidator(
             regex=r'^[a-zA-Z]+$',
             message='Last name must contain only letters'
         )
-    ])
+    ],db_column="last_name"
+    )
 
     email = models.EmailField(max_length=254, unique=True, validators=[
         RegexValidator(
             regex=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
             message='Enter a valid email address'
         )
-    ])
+    ],db_column="email"
+    )
 
     password = models.CharField(max_length=128, validators=[
         RegexValidator(
             regex=r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
             message='Password must be at least 8 characters long and contain at least one letter and one number'
         )
-    ])
+    ],db_column="password"
+    )
 
     phone_number = models.CharField(max_length=15, unique=True, validators=[
         RegexValidator(
             regex=r'^01[0-2,5]{1}[0-9]{8}$',
             message='Phone number must be a valid Egyptian number starting with 010, 011, 012, or 015'
         )
-    ])
-    
+    ],db_column="phone_number"
+    )
     profile_picture = models.ImageField(
         upload_to='media/profile_images/',
         blank=True,
@@ -49,8 +55,9 @@ class Users(models.Model):
             RegexValidator(
                 regex=r'\.(jpg|jpeg|png|gif)$',
                 message='Only image files (jpg, jpeg, png, gif) are allowed',
-                flags=RegexValidator.IGNORECASE
+                flags=re.IGNORECASE
             )
-        ]
+        ],db_column="profile_picture"
     )
+    
     
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import path
+from .models import *
+from .forms import *
 from django.views.generic import View
 
 # Create your views here.
@@ -12,10 +14,12 @@ def sign_in(request):
     return render(request, 'users/sign_in.html')
 
 def sign_up(request):
-    return render(request, 'users/sign_up.html')
+    return render(request, 'users/sign_up.html' )
 
 def account(request):
     return render(request, 'users/account.html')
 
 def profile(request):
     return render(request, 'users/profile.html')
+
+
