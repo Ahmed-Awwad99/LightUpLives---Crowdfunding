e7ed65314f4d078718a1c0c87fcf3c02cda9d637
Ahmed-Awwad99 <ahmed195130@feng.bu.edu.eg>
2025-03-31 16:42:53 +0200

    add password change functionality

--- LightUpLives/settings.py
+++ LightUpLives/settings.py
@@ -144,8 +144,8 @@ STATICFILES_DIRS = ['statics']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 
 
 
--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", index, name="index"),
     path("login/", user_login, name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path("password_change/", auth_views.PasswordChangeView.as_view(template_name="users/password_change_form.html"), name="password_change"),
+    path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name="password_change_done"),
+
 
 ]
