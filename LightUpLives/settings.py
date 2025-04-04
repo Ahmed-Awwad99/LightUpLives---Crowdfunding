fae4b7e237be88e7df12b267c08981a66ac82c72
Salma Tamer <salmatammerr15@gmail.com>
2025-04-04 03:00:35 +0200

    Update static file settings and enhance base template and change styles

--- LightUpLives/settings.py
+++ LightUpLives/settings.py
@@ -136,8 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'statics/'
-STATICFILES_DIRS = ['statics']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
