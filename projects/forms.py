bea32e5448edecb073bc20b5a4ef3f3a0faebd44
Salma Tamer <salmatammerr15@gmail.com>
2025-04-04 03:11:04 +0200

    Add file upload field to ProjectForm and enhance input widgets

--- projects/forms.py
+++ projects/forms.py
@@ -3,12 +3,23 @@ from .models import Project
 from .models import Donation
 
 class ProjectForm(forms.ModelForm):
+    files = forms.FileField(
+        widget=forms.ClearableFileInput(attrs={'multiple': True}),
+        required=False
+    )
+
     class Meta:
         model = Project
-        fields = ['title', 'description', 'category', 'images', 'target', 'tags', 'start_date', 'end_date']
+        fields = ['title', 'description', 'category', 'images', 'target', 'tags', 'start_date', 'end_date', 'files']  # Include 'files' field
         widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date'}),
-            'end_date': forms.DateInput(attrs={'type': 'date'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter project title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Enter project description'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'images': forms.ClearableFileInput(attrs={'multiple': True}),
+            'target': forms.NumberInput(attrs={'class': 'form-control', 'min': '1', 'placeholder': 'Enter target amount'}),
+            'tags': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter tags separated by commas'}),
+            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
         }
 
 class DonationForm(forms.ModelForm):
@@ -16,5 +27,5 @@ class DonationForm(forms.ModelForm):
         model = Donation
         fields = ['amount']
         widgets = {
-            'amount': forms.NumberInput(attrs={'class': 'form-control', 'min': '1'}),
+            'amount': forms.NumberInput(attrs={'class': 'form-control', 'min': '1', 'placeholder': 'Enter donation amount'}),
         }
