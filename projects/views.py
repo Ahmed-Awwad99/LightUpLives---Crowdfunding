97efea1bd59ba1cbd2d37446fbfb84309e1a878f
Salma Tamer <salmatammerr15@gmail.com>
2025-04-06 03:15:44 +0200

    Refactor create_project view to improve form handling and streamline project creation process

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import ProjectForm, DonationForm
-from .models import Project, Donation
+from .forms import ProjectForm, DonationForm, CommentForm, ReportForm
+from .models import Project, Donation, Report, ProjectImage
 
 @login_required
 def create_project(request):
@@ -11,6 +11,9 @@ def create_project(request):
             project = form.save(commit=False)
             project.created_by = request.user
             project.save()
+
+            for image in request.FILES.getlist('images'):
+                ProjectImage.objects.create(project=project, image=image)
             return redirect('home')
     else:
         form = ProjectForm()
@@ -28,25 +31,62 @@ def manage_projects(request):
 def projects_home(request):
     return render(request, 'projects/projects_home.html')
 
-def project_detail(request, project_id):
+def project_detail(request, project_id): 
     project = Project.objects.get(id=project_id)
     donations = project.donations.all()
     total_donated = sum(donation.amount for donation in donations)
+    comments = project.comments.all()
 
     if request.method == "POST":
-        form = DonationForm(request.POST)
-        if form.is_valid():
-            donation = form.save(commit=False)
-            donation.project = project
-            donation.donor = request.user
-            donation.save()
-            return redirect('project_detail', project_id=project.id)
+        if 'donate' in request.POST:
+            form = DonationForm(request.POST)
+            if form.is_valid():
+                donation = form.save(commit=False)
+                donation.project = project
+                donation.donor = request.user
+                donation.save()
+                return redirect('project_detail', project_id=project.id)
+        elif 'comment' in request.POST:
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.project = project
+                comment.user = request.user
+                comment.save()
+                return redirect('project_detail', project_id=project.id)
+        else:
+            form = DonationForm()
+            comment_form = CommentForm()
     else:
         form = DonationForm()
+        comment_form = CommentForm()
 
     return render(request, 'projects/project_detail.html', {
         'project': project,
         'form': form,
+        'comment_form': comment_form,
         'donations': donations,
+        'comments': comments,
         'total_donated': total_donated,
     })
+
+@login_required
+def report_project(request, project_id):
+    project = Project.objects.get(id=project_id)
+    previous_reports = project.reports.all()  # Fetch previous reports for the project
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            report = form.save(commit=False)
+            report.project = project
+            report.user = request.user
+            report.save()
+            return redirect('project_detail', project_id=project.id)
+    else:
+        form = ReportForm()
+    return render(request, 'projects/report_project.html', {
+        'form': form,
+        'project': project,
+        'previous_reports': previous_reports  # Pass previous reports to the template
+    })
+
