4a34a26143c3bcd67896e54b3f9cc2db4d64f505
Salma Tamer <salmatammerr15@gmail.com>
2025-04-09 00:09:43 +0200

    Register ProjectImage model in admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Project, Donation, Comment, Report
+from .models import Category, Project, Donation, Comment, Report, ProjectImage
 
 admin.site.register(Category)
 admin.site.register(Project)
+admin.site.register(ProjectImage)
 admin.site.register(Donation)
 admin.site.register(Comment)
 admin.site.register(Report)
